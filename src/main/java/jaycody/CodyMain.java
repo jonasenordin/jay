9f305896b922ffaf6deb9674e1b841230b6119d8
Jonas N <jonas.j.nordin@gmail.com>
2024-12-11 13:03:40 +0100

    project and main

---
+++ src/main/java/jaycody/CodyMain.java
@@ -0,0 +1,23 @@
+package jaycody;
+
+/**
+ * {@link CodyMain}
+ * 
+ * <pre p>
+ * 
+ * Wed Dec 11 13:03:02 CET 2024
+ * 
+ * Created.
+ * 
+ * </pre>
+ * 
+ * @creatutcms 1733918582696
+ * 
+ * @author jonasnordin5
+ */
+public class CodyMain { //
+	
+	public static void main(String[] args) {
+		System.out.print("Just sayin...");
+	}
+}
